e52969074be50a3d312dc6435e852bd8fd12d2d7
rally19 <masterrally1808@gmail.com>
2024-09-13 14:06:29 +0800

    vantilt3r2rfqat

--- res/scripts/app.js
+++ res/scripts/app.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'res/scripts/particlesjs-config.json', function() {
+particlesJS.load('particles-js', 'res/json/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
